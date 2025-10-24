d335ea39c15415b4204968387b2d34bc6ba2de26
Smbirey <smbirey@gmail.com>
2025-10-24 16:12:51 +0100

    fix

--- app/api/deriv-oauth/route.ts
+++ app/api/deriv-oauth/route.ts
@@ -1,4 +1,3 @@
-import { cookies } from "next/headers";
 import { NextRequest, NextResponse } from "next/server";
 
 import { ROUTES } from "@/lib/constants/routes";
@@ -7,14 +6,18 @@ import { ROUTES } from "@/lib/constants/routes";
 export async function GET(request: NextRequest) {
   const stringifiedSearchParams = request.nextUrl.searchParams.toString();
 
-  const cookieStore = await cookies();
+  const response = NextResponse.redirect(
+    new URL(ROUTES.HANDLE_DERIV, request.url),
+    { status: 303 }
+  );
 
-  cookieStore.set("deriv-accounts", stringifiedSearchParams, {
-    maxAge: 900, // valid for 15mins
+  response.cookies.set("deriv-accounts", stringifiedSearchParams, {
+    maxAge: 900,
     secure: true,
     httpOnly: true,
-    sameSite: "strict",
+    sameSite: "none",
+    path: "/",
   });
 
-  return NextResponse.redirect(new URL(ROUTES.HANDLE_DERIV, request.url));
+  return response;
 }
