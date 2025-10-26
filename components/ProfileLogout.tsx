38f61b2416d36bbeb86775b396743580ee671562
Smbirey <smbirey@gmail.com>
2025-10-26 10:12:52 +0100

    Fix: enable text truncation

--- components/ProfileLogout.tsx
+++ components/ProfileLogout.tsx
@@ -8,8 +8,8 @@ const ProfileLogout = () => {
   const { data } = useSession();
 
   return (
-    <div className="flex flex-col items-start justify-between pb-6">
-      <div className="flex w-full items-center gap-3 pb-2 sm:gap-2">
+    <div className="flex w-full min-w-0 flex-col pb-6">
+      <div className="flex min-w-0 items-center gap-2 pb-2 sm:gap-2">
         <Avatar className="size-7 shrink-0">
           <AvatarImage
             className="h-full w-full rounded-full object-cover"
@@ -17,12 +17,13 @@ const ProfileLogout = () => {
           />
           <AvatarFallback>CN</AvatarFallback>
         </Avatar>
-
         <div className="flex min-w-0 flex-1 flex-col">
-          <h2 className="text-14-medium truncate">{data?.user?.name}</h2>
-
-          <span className="text-12-regular block truncate text-gray-400 sm:text-[10px]">
-            samuelobabire6@gmail.comdjdjjdj
+          {" "}
+          <h2 className="text-14-medium truncate overflow-hidden whitespace-nowrap">
+            {data?.user?.name}
+          </h2>
+          <span className="text-12-regular truncate overflow-hidden whitespace-nowrap text-gray-400 sm:text-[10px]">
+            samuelobabire6@gmail.com
           </span>
         </div>
       </div>
@@ -30,7 +31,7 @@ const ProfileLogout = () => {
       <Separator className="border-accent/10 mb-2 border-1" />
 
       <div className="cursor-pointer" onClick={() => signOut()}>
-        <LogOut className="mr-2 inline" size={20} />{" "}
+        <LogOut className="mr-2 inline" size={20} />
         <span className="text-14-medium">Logout</span>
       </div>
     </div>
