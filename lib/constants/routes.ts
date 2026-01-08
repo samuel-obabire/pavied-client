881c997a2fa8c243f1a028ed9982b48a2f739f76
Smbirey <smbirey@gmail.com>
2026-01-08 15:01:28 +0100

    feat(settings): add default settings redirect and update breakpoint to xl on deriv & bank account setting page

--- lib/constants/routes.ts
+++ lib/constants/routes.ts
@@ -2,6 +2,7 @@ export const ROUTES = {
   HOME: "/",
   CONTACT: "/contact",
   SIGN_IN: "/signin",
+  SETTINGS: "/settings",
   SETTINGS_BIO: "/settings/bio",
   SETUP_DERIV: "/settings/deriv",
   SETUP_BANK: "/settings/bank",
