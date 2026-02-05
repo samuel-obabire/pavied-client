5e4e240e1ebbe9e787960b4adff4a0228dcab01b
Smbirey <smbirey@gmail.com>
2026-02-06 00:06:08 +0100

    fix build issue

--- lib/firebase/firebase.config.ts
+++ lib/firebase/firebase.config.ts
@@ -1,20 +1,9 @@
-import {
-  cert,
-  getApps,
-  initializeApp,
-  type ServiceAccount,
-} from "firebase-admin/app";
+import { cert, getApps, initializeApp } from "firebase-admin/app";
 import { getFirestore } from "firebase-admin/firestore";
 import { getDownloadURL, getStorage } from "firebase-admin/storage";
 import { ENV } from "../env";
 
-let serviceAccount: ServiceAccount;
-
-if (ENV.NODE_ENV === "development") {
-  serviceAccount = require("../../serviceAccountKey.json") as ServiceAccount;
-} else {
-  serviceAccount = JSON.parse(ENV.FIREBASE_SERVICE_ACCOUNT);
-}
+const serviceAccount = JSON.parse(ENV.FIREBASE_SERVICE_ACCOUNT);
 
 if (!getApps().length) {
   initializeApp({
@@ -23,7 +12,7 @@ if (!getApps().length) {
   });
 }
 
-const firestoreDb = getFirestore();
+const db = getFirestore();
 const bucket = getStorage().bucket();
 
-export { firestoreDb, bucket, getDownloadURL };
+export { db, bucket, getDownloadURL };
