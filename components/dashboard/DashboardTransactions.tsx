09291104d170c0b1e79672e146ad3829d72fbfaa
Tori-dev <victory.codess@gmail.com>
2026-02-06 10:37:48 +0100

    dashboard transactions background fix

--- components/dashboard/DashboardTransactions.tsx
+++ components/dashboard/DashboardTransactions.tsx
@@ -8,7 +8,7 @@ const DashboardTransactions = async ({ userId }: { userId: string }) => {
 
   return (
     <article className="mt-8 flex-1 overflow-visible rounded-2xl pt-0">
-      <header className="sticky top-0 left-0 z-10 mb-4 flex items-center justify-between px-2 py-2">
+      <header className="sticky top-0 left-0 z-10 mb-4 flex items-center justify-between px-2 py-2 bg-white_dark-black-1">
         <h2 className="text-20-medium text-black-1 dark:text-white">Recent Transactions</h2>
         <Link 
           href={ROUTES.TRANSACTIONS}
