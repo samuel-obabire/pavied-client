201bf2f9b7eb57d7358aaeecc9f029c994102bfc
Smbirey <smbirey@gmail.com>
2026-02-07 13:13:54 +0100

    derive storagebucket from env

--- lib/firebase/firebase.config.ts
+++ lib/firebase/firebase.config.ts
@@ -5,11 +5,12 @@ import { getDownloadURL, getStorage } from "firebase-admin/storage";
 const serviceAccount = JSON.parse(
   process.env.FIREBASE_SERVICE_ACCOUNT as string,
 );
+const storageBucket = process.env.FIREBASE_STORAGE_BUCKET;
 
 if (!getApps().length) {
   initializeApp({
     credential: cert(serviceAccount),
-    storageBucket: "pavied-telegram.firebasestorage.app",
+    storageBucket,
   });
 }
 
